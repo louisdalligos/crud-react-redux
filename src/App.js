84eb7aeba6883c5aa89b641b9df21040b98ac8de
Louis Dalligos <zindex14@gmail.com>
2018-07-01 10:42:39 +0800

    Connect gamepage component to redux and render empty message on empty collection

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <Link to="games">Games</Link>
-        </p>
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+                <Link to="games">Games</Link>
+            </p>
 
-        <Route path="/games" component={GamesPage} />
-      </div>
-    );
-  }
+            <Route path="/games" component={GamesPage} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/GamesPage.js
+++ src/GamesPage.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import GamesList from './GamesList';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class GamesPage extends React.Component {
     render() {
         return (
             <div>
                 <h1>Games List</h1>
+
+                <GamesList games={this.props.games} />
             </div>
         );
     }
 }
 
-export default GamesPage;
+GamesPage.protoTypes = {
+    games: PropTypes.array.isRequired
+}
+
+function mapStateToProps(state) {
+    return {
+        games: state.games
+    }
+}
+
+export default connect(mapStateToProps)(GamesPage);
