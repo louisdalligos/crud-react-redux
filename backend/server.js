7e8f2f7fa88d5f9c5ff270d47c9630731794f416
Louis Dalligos <zindex14@gmail.com>
2018-07-02 09:25:00 +0800

    Add our routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import games from './routes/games.routes';
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -11,3 +13,6 @@ app.get('/', (req, res) => res.send('Hello world'));
 app.listen(3005, () => {
     console.log('server started on port 3005');
 });
+
+
+app.use('/api', games);
