70cc09660f81ed8e6ef03a794e69e66746eaf5f6
Louis Dalligos <zindex14@gmail.com>
2018-07-02 15:21:27 +0800

    Add our LIST_GAMES action

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,16 @@
+export const LIST_GAMES = 'LIST_GAMES';
+
+export function listGames(games) {
+    return {
+        type: LIST_GAMES,
+        games
+    }
+}
+
 export function fetchGames() {
     return dispatch => {
-        fetch('/api/games');
+        fetch('/api/games')
+            .then(res => res.json())
+            .then(data => dispatch(listGames(data.games)));
     }
 }
--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,5 +1,9 @@
+import { LIST_GAMES } from "../actions";
+
 export default function games(state = [], action = {}) {
     switch(action.type) {
+        case LIST_GAMES:
+            return action.games;
         default: return state;
     }
 }
